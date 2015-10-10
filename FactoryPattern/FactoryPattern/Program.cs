2f1b7f0f768b8fef44e5278d852077bac1e3782f
Soe Than <soethan@gmail.com>
2015-10-10 22:47:40 +0800

    initial commit

---
+++ FactoryPattern/FactoryPattern/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var child = new Child();
+            Console.Read();
+        }
+    }
+
+    public class Parent
+    {
+        private string strParent = "aaa";
+        public Parent()
+        {
+            Console.WriteLine("Parent Constructor");
+        }
+    }
+    public class Child : Parent
+    {
+        private string strChild = "bbb";
+        public Child():this("haha")
+        {
+            Console.WriteLine("Child Constructor");
+        }
+        public Child(string param1)
+        {
+            Console.WriteLine("Child Constructor 2 " + param1);
+        }
+    }
+}
